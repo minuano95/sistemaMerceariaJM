b2e3f4bd69776f640e69ba3392436d8677f803e3
Joao Ruiz <minuano95@gmail.com>
2025-03-27 08:08:11 -0300

    Alterações na db

--- backend/produtos/models.py
+++ backend/produtos/models.py
@@ -14,7 +14,7 @@ class Produto(models.Model):
     codigo = models.BigIntegerField(unique=True)
     nome = models.CharField(max_length=100)
     preco_custo = models.DecimalField(max_digits=7, decimal_places=2)
-    preco_venda = models.DecimalField(max_digits=7, decimal_places=2)
+    margem_lucro = models.IntegerField()
     estoque = models.IntegerField()
     data_cadastro = models.DateField(auto_now_add=True)
     data_atualizacao = models.DateField(auto_now=True)
@@ -22,6 +22,9 @@ class Produto(models.Model):
     promocao = models.BooleanField(default=False)
     situacao = models.CharField(max_length=20, choices=[('ATIVO', 'Ativo'), ('INATIVO', 'Inativo')])
     desconto = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    
+    def preco_final(self):
+        return self.preco_custo + (self.preco_custo * self.margem_lucro / 100)
     
     def __str__(self):
         return f'{self.codigo} {self.nome}'
